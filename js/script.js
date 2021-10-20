92f33001f7b0f18e87dd8ea9a2515d9521f2c5f2
Diogo <diogo_1257@hotmail.com>
2021-10-20 09:22:23 -0300

    Operadores Lógicos

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// OPERADOR LÓGICO OR
+
+/*
+var nome = "s1mple";
+var idade = 25;
+
+var nome1 = "fallen";
+var idade2 = 35;
+
+
+if (nome == "s1mple" && idade == 25 ) {
+
+	console.log("He play for NATUS VINCERE");
+}else {
+
+console.log("Random Player :D")
+
+}
+
+*/
+
+// OPERADOR OR ||
+
+var nome1 = "fallen";
+var idade2 = 35;
+
+if  (nome1 == "fallen" || idade2 == 45) {
+
+console.log("He plays for liquid");
+
+} else {
+
+console.log("I've never heard about this player");
+
+}
